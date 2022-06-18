299ede4eca8f948980cdaa274bb06f5b7ea173ab
duykhangdangzn1 <101445598+duykhangdangzn1@users.noreply.github.com>
2022-06-18 22:44:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     question: 'Nhà anh rất giàu đủ để em sống cả đời nên hãy chốt đơn đi nhé <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Nhà anh rất giàu đủ để em sống cả đời nên hãy chốt đơn đi nhé ',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
