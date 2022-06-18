7e38be6b7724bc0d4b66a3873c28447d19b3df92
duykhangdangzn1 <101445598+duykhangdangzn1@users.noreply.github.com>
2022-06-18 22:42:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Nhà anh rất giàu đủ để em sống cả đời nên hãy chốt đơn đi nhé <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
